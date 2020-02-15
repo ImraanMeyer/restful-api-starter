e6a670eeff43b754fe7eff337ddd78440f2311f0
ImraanMeyer <imraan.meyer97@gmail.com>
2020-02-15 13:30:58 +0200

    .gitignore update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
+
 let { writeData, content } = require("../helpers/helpers");
 
 router.get("/", (req, res) => {
